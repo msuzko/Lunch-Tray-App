df55490b6896d37c2817063654783f92b7621ded
Mikhail Suzko <msuzko@gmail.com>
2025-08-24 10:20:05 -0600

    Add enum for screens, add AppBar

--- app/src/main/java/com/mec/lunchtray/LunchTrayScreen.kt
+++ app/src/main/java/com/mec/lunchtray/LunchTrayScreen.kt
@@ -15,22 +15,69 @@
  */
 package com.mec.lunchtray
 
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.mec.lunchtray.ui.OrderViewModel
 
-// TODO: Screen enum
-
-// TODO: AppBar
+enum class Screen(@param:StringRes val title: Int) {
+    Start(R.string.start_order),
+    EntreeMenu(R.string.choose_entree),
+    SideDishMenu(R.string.choose_side_dish),
+    AccompanimentMenu(R.string.choose_accompaniment),
+    Checkout(R.string.order_checkout)
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LunchTrayApp() {
-    // TODO: Create Controller and initialization
+fun LunchTrayAppBar(
+    currentScreen: Screen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = {Text(stringResource(id = currentScreen.title))},
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LunchTrayApp(
+    navController: NavHostController = rememberNavController()
+) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = Screen.valueOf(backStackEntry?.destination?.route ?: Screen.Start.name)
 
     // Create ViewModel
     val viewModel: OrderViewModel = viewModel()
